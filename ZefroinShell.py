c876f216e3945e36f1bf412108ac6f4715db6c13
Azure <74486606+AzureianGH@users.noreply.github.com>
2022-10-19 15:56:37 -0400

    Update 1.2
    Large Change

--- ZefroinShell.py
+++ ZefroinShell.py
@@ -1,118 +1,204 @@
-print("Zefroin Shell 1.0 COPYRIGHT OF AZUREIAN")
-error109 = "No Statement provided! 109"
-error1 = "Unknown Statement! 1"
+
+try:
+  import time
+  import os
+  import socket
+  import subprocess
+  from update_check import update
+  import urllib
+  from tqdm import tqdm
+  from pythonping import ping
+except Exception as e:
+  print(e)
+  print("Did you install the requirements.txt?")
+  time.sleep(5)
+  quit()
+updtelog = False
+seconds = 7
+print("Checking on updates for Zefroin...")
+for i in tqdm(range(seconds)):
+    time.sleep(1)
+from update_check import isUpToDate
+
+if isUpToDate(__file__, "https://raw.githubusercontent.com/AzureianGH/Zefroin-Shell/main/ZefroinShell.py") == False:
+   updte = input("Update available, would you like to update?\n")
+   if updte == "Yes":
+      update(__file__, "https://raw.githubusercontent.com/AzureianGH/Zefroin-Shell/main/ZefroinShell.py")
+      updtelog = True
+   if updtelog == True:
+    with open(r'\zefdump.txt', 'w') as fp:
+      fp.write('Zefroin updated.')
+seconds = 60
+print("Mounting Zefroin...")
+for i in tqdm(range(seconds)):
+    time.sleep(0.1)
+with open(r'\zefdump.txt', 'w') as fp:
+    fp.write('Dump file created.')
+    pass
+print("Mounted.")
+seconds = 30
+print("Starting Zefroin...")
+for i in tqdm(range(seconds)):
+    time.sleep(0.01)
+print("\033[1;33;40m Zefroin Shell 1.2 COPYRIGHT OF AZUREIAN")
+print("\033[1;33;40m Powered by Python, tqdm, pythonping!")
+error109 = "\033[1;31;40m No Statement provided! 109"
+error1 = "\033[1;31;40m Unknown Statement! 1"
 error56a = 0
-error32 = "Unable to parse negative value! 32"
-error18 = "Value input can't be negative! 18"
-error0 = "No Feature! 0"
-warning1 = "Warning! This feature is experimental and may result in loops or possible breaks from the code!"
-errorunk = "Unknown error has occured! UNK"
-error45 = "No sign entered! 45"
+error32 = "\033[1;31;40m Unable to parse negative value! 32"
+error18 = "\033[1;31;40m Value input can't be negative! 18"
+error0 = "\033[1;31;40m No Feature! 0"
+warning1 = "\033[1;31;40m Warning! This feature is experimental and may result in loops or possible breaks from the code!"
+errorunk = "\033[1;31;40m Unknown error has occured! UNK"
+error45 = "\033[1;31;40m No sign entered! 45"
 iftrue = 1
+error67 = "\033[1;31;40m Value can not be letters! 67"
 while True:
-    zinput = input("Zef >> ")
-    if zinput == "help":
-        print("help - Opens Help
-print - Prints a line
-loop - Loops a word for a certain amount
-math - Simple Mathmatics")
-    elif zinput == "print":
-        prnt = input("Print >> ")
-        print(prnt)
-    elif zinput == "":
-        print(error109)
-    
-    elif zinput == "loop":
-        intimes = 0
-        loop = input("Loop >> ")
-        times = input("How many times? >> ")
-        intimes = int(times)
-        if intimes > 1000:
-            error56a = intimes - 1000
-            error56b = str(error56a)
-            error56 = "Charcter Limit Exceeded by " + error56b
-            print(error56 + "! 56")
-            intimes = 0
-            addtimes = 0
-        elif intimes < 0:
-            print(error32)
-            intimes = 0
-            addtimes = 0
-        elif intimes == -0:
-            print(error18)
-            intimes = 0
-            addtimes = 0
-            
-        addtimes = 0
+    while True:
+        zinput = input("\033[1;32;40mZef >> ")
+        if zinput == "help":
+            print("help - Opens Help\nprint - Prints a line\nloop - Loops a word for a certain amount\nmath - Simple Mathmatics")
+        elif zinput == "print":
+            prnt = input("Print >> ")
+            print(prnt)
+        elif zinput == "":
+            print(error109)
         
-        while addtimes < intimes:
-            addtimes = addtimes + 1
-            print(loop)
-    elif zinput == "math":
-        print(error0)
-        print(warning1)
-        print("What operation?
-+
--
-*
-/")
-        mathop = input("Math >> ")
-        mathoper = 0
-        if mathop == "+":
-            mathoper = 1
-            iftrue = 1
-        elif mathop == "-":
-            mathoper = 2
-            iftrue = 1
-        elif mathop == "*":
-            mathoper = 3
-            iftrue = 1
-        elif mathop == "/":
-            mathoper = 4
-            iftrue = 1
-        else:
-            mathoper = 5
-        if mathoper == 0:
-            print(errorunk)
-            iftrue = 0
-        elif mathoper == 5:
-            iftrue = 0
-            print(error45)
-        if iftrue == 1:
-            print("Number 1: ")
-            math1 = input("Math >> ")
-            print("Number 2: ")
-            math2 = input("Math >> ")
-            math1i = int(math1)
-            math2i = int(math2)
-            mathp = math1i + math2i
-            mathm = math1i - math2i
-            mathmt = math1i * math2i
-            mathd = math1i / math2i
-            mathper = math1i % math2i
-            if mathoper == 1:
-                print(mathp)
-                # +
-            elif mathoper == 2:
-                print(mathm)
-                # -
-            elif mathoper == 3:
-                print(mathmt)
-                if math1i == math2i:
-                    math1m = str(math1i)
-                    math2m = str(math2i)
-                    mathmt1 = str(mathmt)
-                    print(mathmt1 + " is the square of " + math1m + " and " + math2m)
-                # *
-            elif mathoper == 4:
-                print(mathd)
-                if mathd == math2i:
-                    mathds = str(mathd)
-                    math1s = str(math1i)
-                    print(mathds + " is the square root of " + math1s)
-                # /
+        elif zinput == "loop":
+            intimes = 0
+            loop = input("Loop >> ")
+            times = input("How many times? >> ")
+            if times.isnumeric() == False:
+                print(error67)
+                break
+                
+            else: 
+                intimes = int(times)
+                if intimes > 1000:
+                    error56a = intimes - 1000
+                    error56b = str(error56a)
+                    error56 = "Charcter Limit Exceeded by " + error56b
+                    print(error56 + "! 56")
+                    intimes = 0
+                    addtimes = 0
+                elif intimes < 0:
+                    print(error32)
+                    intimes = 0
+                    addtimes = 0
+                elif intimes == -0:
+                    print(error18)
+                    intimes = 0
+                    addtimes = 0
+                
+                addtimes = 0
             
+                while addtimes < intimes:
+                    addtimes = addtimes + 1
+                    print(loop)
+        elif zinput == "math":
+            print("What operation?\n+\n-\n*\n/")
+            mathop = input("Math >> ")
+            mathoper = 0
+            if mathop == "+":
+                mathoper = 1
+                iftrue = 1
+            elif mathop == "-":
+                mathoper = 2
+                iftrue = 1
+            elif mathop == "*":
+                mathoper = 3
+                iftrue = 1
+            elif mathop == "/":
+                mathoper = 4
+                iftrue = 1
             else:
+                mathoper = 5
+            if mathoper == 0:
                 print(errorunk)
-    else:
-        print(error1)
+                iftrue = 0
+            elif mathoper == 5:
+                iftrue = 0
+                print(error45)
+            if iftrue == 1:
+                print("Number 1: ")
+                math1 = input("Math >> ")
+                print("Number 2: ")
+                math2 = input("Math >> ")
+                if math1.isnumeric() == False:
+                    print(error67)
+                    break
+                else:
+                    if math2.isnumeric() == False:
+                        print(error67)
+                        break
+                    else:
+                        math1i = int(math1)
+                        math2i = int(math2)
+                        mathp = math1i + math2i
+                        mathm = math1i - math2i
+                        mathmt = math1i * math2i
+                        mathd = math1i / math2i
+                        mathper = math1i % math2i
+                        if mathoper == 1:
+                            print(mathp)
+                            if mathp == 38:
+                                mathazure = str(mathp)
+                                print(mathazure + " Is Azureian's favorite number!")
+                            # +
+                        elif mathoper == 2:
+                            print(mathm)
+                            # -
+                        elif mathoper == 3:
+                            print(mathmt)
+                            if math1i == math2i:
+                                math1m = str(math1i)
+                                math2m = str(math2i)
+                                mathmt1 = str(mathmt)
+                                print(mathmt1 + " is the square of " + math1m + " and " + math2m)
+                            # *
+                        elif mathoper == 4:
+                            print(mathd)
+                            if mathd == math2i:
+                                mathds = str(mathd)
+                                math1s = str(math1i)
+                                print(mathds + " is the square root of " + math1s)
+                            # /
+                        
+                        else:
+                            print(errorunk)
+        elif zinput == "end":
+            quit()
+        elif zinput == "msdos":
+              print("  __  __  _____       _____   ____   _____  ")
+              print("  |  \/  |/ ____|     |  __ \ / __ \ / ____|")
+              print("  | \  / | (___ ______| |  | | |  | | (___  ")
+              print("  | |\/| |\___ \______| |  | | |  | |\___ \ ")
+              print("  | |  | |____) |     | |__| | |__| |____) |")
+              print("  |_|  |_|_____/      |_____/ \____/|_____/ ")
+              print("                                            ")
+              input("C:\>")
+              print("Bad command or not swag enough")
+        elif zinput == "kcal":
+          print("How many Kcals?")
+          kcalinput = input("Kcal >> ")
+          if kcalinput.isnumeric == False:
+            print(error67)
+            break
+          else:
+            kcal = float(kcalinput)
+            kcal2 = kcal * .10
+            kcal3 = kcal2 * .10
+            kcal4 = kcal3 * .10
+            kcal5 = kcal4 * .10
+  
+  
+            kcal1s = str(kcal2)
+            kcal2s = str(kcal3)
+            kcal3s = str(kcal4)
+            kcal4s = str(kcal5)
+  
+  
+            
+            print(kcalinput + " - Producers\n" + kcal1s + " - Primary Consumers\n" + kcal2s + " - Secondary Consumers\n" + kcal3s + " - Tertiary Consumers\n" + kcal4s + " - Apex Predators")
+        else:
+            print(error1)
