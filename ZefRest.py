2e8e9a712f44230dc17adf6e96bd0e52e605c773
Azure <74486606+AzureianGH@users.noreply.github.com>
2022-10-20 21:44:26 -0400

    Update ZefRest.py

--- ZefRest.py
+++ ZefRest.py
@@ -1,9 +1,13 @@
-import tqdm
+from tqdm import tqdm
 import time
+from subprocess import Popen
 
 print("Restarting...")
 seconds = 12
 for i in tqdm(range(seconds)):
     time.sleep(0.1)
-exec(open('ZefroinShell.py').read())
+def run(runfile):
+  with open(runfile,"r") as rnf:
+    exec(rnf.read())
+run("ZefroinShell.py")
 quit()
